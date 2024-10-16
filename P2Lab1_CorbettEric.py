d32c3a0c157b955a0ca026d2abd3af2db261ae0b
Erictogoated <corbette4431@student.faytechcc.edu>
2024-10-16 01:00:21 -0400

    Add files via upload

---
+++ P2Lab1_CorbettEric.py
@@ -0,0 +1,40 @@
+ # Eric Corbett
+ # 10/2/2024
+ #P2Lab1
+ # Using Lab expressions
+import math
+
+ #Get radius user
+radius = float(input("enter the radius:"))
+
+# Calculate circum, diameter, area
+cir = 2 * math.pi * radius
+diam = 2 * radius
+area = math.pi * (radius ** 2)
+#display results
+print(f" the diametr of the circle is {diam:.1f}")
+print(f"the circumference of the circle is {cir:.2f}")
+print(f"the area of the circle is {area:3f}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
